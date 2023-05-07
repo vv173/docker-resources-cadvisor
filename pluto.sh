653861dfc2033318701fc028885476c1e690f4fd
Viktor Vodnev <v.vodnev73@gmail.com>
2023-05-07 20:34:22 +0200

    Add total memory output to the script

--- pluto.sh
+++ pluto.sh
@@ -3,9 +3,12 @@
 # Get the date and time
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Get the amount of free memory
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+# Get the amount of used memory
+USED_MEMORY=$(free -h --mega | awk '/^Mem/ {printf "%.2f%%", $3*100/$2 }')
+
+# Get the total amount of memory
+TOTAL_MEMORY=$(free -h --mega | awk '/^Mem/ {printf $2}')
 
 # Write the data to the log file
-echo "${DATE} | Free Memory: ${MEMORY}" >> info.log
+echo "${DATE} | Used Memory: ${USED_MEMORY} | Total Memory: ${TOTAL_MEMORY}" >> info.log
 
